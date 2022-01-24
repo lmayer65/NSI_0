cdb095bbbbcaab68f35b48c2ffcbd44d7b8428a1
Laurent <60965083+lmayer65@users.noreply.github.com>
2022-01-24 19:27:38 +0100

    Add files via upload

--- Projets/PRJ_BDD/PRJ_BDD_Introduction.sql
+++ Projets/PRJ_BDD/PRJ_BDD_Introduction.sql
@@ -1,16 +1,26 @@
-CREATE TABLE IF NOT EXISTS Agences (
+CREATE TABLE IF NOT EXISTS agences (
 	id_agence INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	ville VARCHAR(30) COLLATE UTF8mb4_UNICODE_CI,
 	dirigeant VARCHAR(30) COLLATE UTF8mb4_UNICODE_CI,
 	flotte INT
 )
 ENGINE = InnoDB;
 
 
-CREATE TABLE IF NOT EXISTS Voitures(
-	id_enseignant INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS voitures(
+	id_voiture INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	id_agence INT NOT NULL,
 	constructeur VARCHAR(30) COLLATE UTF8mb4_UNICODE_CI,
 	annee INT,
+	carburant VARCHAR(30) COLLATE UTF8mb4_UNICODE_CI,
 	FOREIGN KEY(id_agence) REFERENCES Agences(id_agence)
 )
-ENGINE = InnoDB;
+ENGINE = INNODB;
+
+
+INSERT INTO agences(ville,dirigeant,flotte) VALUES
+	("Paris_I","Dupont",55),
+	("Lyon","Ferrière",82),
+	("Paris_XII","Dupont",103),
+	("Bordeaux","Durand",41),
+	("Tours","Bertrand",39);
